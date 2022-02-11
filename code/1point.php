9f0d16350c6f8fa9b574d26e20c45e9cd893c102
Tawoker <jhoshkyrbee1@gmail.com>
2022-02-11 12:40:28 +0200

    rename index.php to 1point.php, did 2 point

--- code/1point.php
+++ code/1point.php
@@ -1,6 +1,5 @@
 <?php
 $very_bad_unclear_name = "15 chicken wings";
-
 $other = & $very_bad_unclear_name;
 $other .= " in basket";
 echo $other;
---
+++ code/2point.php
@@ -0,0 +1,9 @@
+<?php
+$x_int = 1337;
+echo $x_int, "\n";
+$y_float = 13.37;
+echo $y_float, "\n";
+echo 5 + 7, "\n";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month, "\n";
